e1182e427a5f993c1cfcf347a6e86bd4c16aba31
Rorik <rorik302@gmail.com>
2020-11-16 15:26:05 +0300

    primeflex

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+function App() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default App;
